4e1d5cfdc229f289841b3d029ed22e4449648639
karpenko.rostyslav <rostyslav.karpenko@gen.tech>
2024-12-30 03:28:35 +0200

    records crd

--- app/views.py
+++ app/views.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 
 USERS_DATA_FILE = "app/users.json"
 CATEGORIES_DATA_FILE = "app/categories.json"
+RECORDS_DATA_FILE = "app/records.json"
 
 
 def load_data(data_filepath):
@@ -22,6 +23,7 @@ def save_data(data, data_filepath):
 
 users = load_data(USERS_DATA_FILE)
 categories = load_data(CATEGORIES_DATA_FILE)
+records = load_data(RECORDS_DATA_FILE)
 @app.route("/healthcheck")
 def healthcheck():
     return "<p>healthcheck</p>"
@@ -79,3 +81,58 @@ def delete_category_by_id(category_id: str):
             categories.remove(element)
             save_data(categories,CATEGORIES_DATA_FILE)
             return json.encoder.encode_basestring(element["id"])
+
+
+# // record
+@app.post("/record")
+def create_record():
+    record_data = request.get_json()
+    record_id = uuid.uuid4().hex
+    record = {"id": record_id, **record_data}
+    records.append(record)
+    save_data(records, RECORDS_DATA_FILE)
+    return record
+@app.get("/record/<record_id>")
+def get_record_by_id(record_id: str):
+    for element in list(records):
+        if element["id"] == record_id:
+            return element
+
+@app.delete("/record/<record_id>")
+def delete_record_by_id(record_id: str):
+     for element in list(records):
+         if element["id"] == record_id:
+            records.remove(element)
+            save_data(records,RECORDS_DATA_FILE)
+            return json.encoder.encode_basestring(element["id"])
+
+
+
+@app.get("/record")
+def get_records_filter_by_category_or_user():
+    category_id =  request.args.get('category_id')
+    user_id =  request.args.get('user_id')
+
+    if category_id is None and user_id is None:
+        raise Exception("category_id or user_id is required")
+
+    if category_id is not None and user_id is None:
+        arr = []
+        for element in list(records):
+            if element["category_id"] == category_id:
+                arr.append(element)
+        return arr
+
+    if user_id is not None and category_id is None:
+        arr = []
+        for element in list(records):
+            if element["user_id"] == user_id:
+                arr.append(element)
+        return arr
+
+    if category_id is not None and user_id is not None:
+        arr = []
+        for element in list(records):
+            if element["category_id"] == category_id and element["user_id"] == user_id:
+                arr.append(element)
+        return arr
