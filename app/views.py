6c21962dfb778359ccb540967b70c2baa8ae2a11
karpenko.rostyslav <rostyslav.karpenko@gen.tech>
2024-12-30 02:30:54 +0200

    json-file load/save operation

--- app/views.py
+++ app/views.py
@@ -1,6 +1,18 @@
 from flask import Flask
+import json
 
 app = Flask(__name__)
+
+def load_data(data_filepath):
+    try:
+        with open(data_filepath, "r") as file:
+            return json.load(file)
+    except FileNotFoundError as e:
+        return {}
+
+def save_data(data, data_filepath):
+    with open(data_filepath, "w") as file:
+        json.dump(data, file, indent=4)
 
 @app.route("/healthcheck")
 def healthcheck():
